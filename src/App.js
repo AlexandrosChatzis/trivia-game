9e65b9145f8e56d299cd04a073a34c238df695eb
Alexandros Chatzis <Alexandros_chatzis@yahoo.com>
2022-11-06 16:31:32 +0200

    setup pages and router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import Welcome from "./pages/Welcome";
+import Questions from "./pages/Questions";
+import FinalScore from "./pages/FinalScore";
+import LeaderBoard from "./pages/LeaderBoard";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/finalscore" element={<FinalScore />} />
+          <Route path="/leaderboard" element={<LeaderBoard />} />
+          <Route path="*" element={<Welcome />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
